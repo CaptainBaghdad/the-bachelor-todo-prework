4531e934e5a938d6de8bd0a7dcf7f49fea16b4fc
Jason Pitts <juniorjr083@gmail.com>
2018-06-14 22:43:03 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,13 +1,36 @@
-def get_first_name_of_season_winner(data, season)
-  # code here
+def get_first_name_of_season_winner(data,season)
+  data[season].each do |contestant|
+    if contestant["status"] == "Winner"
+      return contestant["name"].split(" ")[0]
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
+  ans = ""
+  data.each do |k,array_of_hashes|
+   array_of_hashes.find do |occ|
+   if occ["occupation"] == occupation
+   ans = occ["name"]
+   end 
+   end
+   end
   # code here
+  ans
 end
 
 def count_contestants_by_hometown(data, hometown)
+  ans = []
+  data.each do |k,array_of_hashes|
+   array_of_hashes.find do |occ|
+   if occ["hometown"] == hometown
+   ans << hometown
+   end 
+   end
+   end
   # code here
+  ans.length
+  
 end
 
 def get_occupation(data, hometown)
